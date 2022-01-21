f95325a3f09a9ffca735b46ce29b5482a35eef67
maxy4 <42559043+mxy4@users.noreply.github.com>
2022-01-21 08:35:41 -0600

    test

--- connect.py
+++ connect.py
@@ -19,18 +19,18 @@ def connect():
 	# execute a statement
         print('PostgreSQL database version:')
         cur.execute('SELECT version()')
-        cur.execute('SELECT * FROM windows')
+        cur.execute('SELECT * FROM loldb.windows')
 
         # display the PostgreSQL database server version
         db_version = cur.fetchone()
         print(db_version)
        
 	# close the communication with the PostgreSQL
-        cur.close()
+        cur.close() 
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
-        if conn is not None:
+        if conn is not None: 
             conn.close()
             print('Database connection closed.')
 
